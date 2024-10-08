11e3032d3e6b47a573c073d83689981da0e80f92
Matt Anielski <anielskim@yahoo.com>
2024-10-08 00:58:11 -0400

    Update to tab (4) spacing

--- js/cookie-banner.js
+++ js/cookie-banner.js
@@ -1,22 +1,23 @@
-	// Cookie banner
-	document.addEventListener("DOMContentLoaded", function() {
-		const cookieBanner = document.getElementById("cookieBanner");
-		const acceptCookies = document.getElementById("acceptCookies");
-		const declineCookies = document.getElementById("declineCookies");
+// Cookie banner
+document.addEventListener("DOMContentLoaded", function () {
+    const cookieBanner = document.getElementById("cookieBanner");
+    const acceptCookies = document.getElementById("acceptCookies");
+    const declineCookies = document.getElementById("declineCookies");
 
-		// Check if cookies are already accepted
-		if (true) { //!localStorage.getItem("cookiesAccepted")
-			cookieBanner.classList.add("show");
-		}
+    // Check if cookies are already accepted
+    if (true) {
+        //!localStorage.getItem("cookiesAccepted")
+        cookieBanner.classList.add("show");
+    }
 
-		// Accept Cookies
-		acceptCookies.addEventListener("click", function() {
-			localStorage.setItem("cookiesAccepted", "true");
-			cookieBanner.classList.remove("show");
-		});
+    // Accept Cookies
+    acceptCookies.addEventListener("click", function () {
+        localStorage.setItem("cookiesAccepted", "true");
+        cookieBanner.classList.remove("show");
+    });
 
-		// Decline Cookies
-		declineCookies.addEventListener("click", function() {
-			cookieBanner.classList.remove("show");
-		});
-	});
+    // Decline Cookies
+    declineCookies.addEventListener("click", function () {
+        cookieBanner.classList.remove("show");
+    });
+});
--- js/navbar.js
+++ js/navbar.js
@@ -1,39 +1,24 @@
-	// Toggle menu for mobile
-	const navToggle = document.querySelector('.nav-toggle');
-	const navLinks = document.querySelector('.nav-links');
+// Toggle menu for mobile
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".nav-links");
 
-	navToggle.addEventListener('click', function() {
-		navLinks.classList.toggle('open');
-	});
+navToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("open");
+});
 
-	// Close nav-toggle when screen size changes
-	window.addEventListener('resize', function() {
-	if (window.innerWidth > 768) {
-		navLinks.classList.remove('open');
-	}
-	});
+// Close nav-toggle when screen size changes
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 768) {
+        navLinks.classList.remove("open");
+    }
+});
 
-
-	
-
-
-
-	// window.addEventListener("DOMContentLoaded", function () {
-	// 	// Store and restore scroll position
-	// 	if (sessionStorage.getItem("scrollPosition")) {
-	// 	  window.scrollTo(0, parseInt(sessionStorage.getItem("scrollPosition"), 10));
-	// 	}
-	  
-	// 	// Save the current scroll position before the page unloads
-	// 	window.addEventListener("beforeunload", function () {
-	// 	  sessionStorage.setItem("scrollPosition", window.scrollY);
-	// 	});
-	  
-	// 	// Adjust behavior when scrolled to the bottom of the page
-	// 	window.addEventListener("scroll", function () {
-	// 	  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-	// 		sessionStorage.setItem("scrollPosition", window.scrollY);
-	// 	  }
-	// 	});
-	//   });
-	  
+// Close nav-toggle when a link is clicked
+const navLinkItems = document.querySelectorAll(".nav-links a");
+navLinkItems.forEach((link) => {
+    link.addEventListener("click", () => {
+        if (navLinks.classList.contains("open")) {
+            navLinks.classList.remove("open");
+        }
+    });
+});
